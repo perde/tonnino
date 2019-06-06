ca39df94d592263ef210ac9f2f17774d6c4fd563
Percy <perde29@gmail.com>
2019-06-06 15:11:13 -0500

    tonnino

---
+++ index.php
@@ -0,0 +1,153 @@
+<!DOCTYPE HTML>
+<html lang="es">
+<head>
+ <title>Tonnino</title>
+ <meta charset="UTF-8">
+ <meta name="description" content="Tonnino" >
+ <link href="bootstrap/css/bootstrap.min.css" media="screen" rel="stylesheet" type="text/css">
+ <link href="css/style.css" media="screen" rel="stylesheet" type="text/css">
+</head>
+<body>
+ 
+  <div class="container-header" >
+    
+    <header>
+     <div class="col-md-2 padding-top-0" style="text-align:center;" >
+      <img src="img/logom.jpg" class="logo-general" />   
+      <hr class="class-padding-bottom" />
+     </div>
+     <div class="col-md-10 padding-top-0 " >
+      <!-- --> <!-- --> <!-- -->
+      <nav class="navbar navbar-default">
+        <div class="navbar-header">
+         <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+         </button>
+        </div>
+        <div class="collapse navbar-collapse " id="bs-example-navbar-collapse-1 ">
+         <ul class="nav navbar-nav">
+           <li><a href="#"> SOSTENIBILIDAD </a></li>
+           <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">PRUEBA TONNINO <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+             <li><a href="#">RECETAS</a></li>
+             <li><a href="#">PORTAFOLIO</a></li>
+             <li><a href="#">NUESTRO PROCESO</a></li>
+            </ul>
+           </li>
+           <li><a href="#">DONDE COMPRAR</a></li>
+           <li><a href="#">CONTACTANOS</a></li>
+         </ul>
+            
+         <ul class="nav navbar-nav navbar-right">
+          <li><a href="#" class="class-trace" > TRACE REGISTER </a></li>
+          <li> <img src="img/redes1.png" style="padding:12px;" /> </li>
+          <li> <img src="img/redes2.png" style="padding:12px;" /> </li>
+          <li class="paddin-separation">  </li>
+         </ul>   
+        </div><!-- /.navbar-collapse -->
+       </nav> 
+       <!-- --> <!-- --> <!-- -->   
+     </div>  
+    </header>  
+      
+    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+          <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+          <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+          <div class="item active">
+           <img src="img/portada.png" alt="Poratada 1" class="img-responsive" >
+           <div class="carousel-caption">
+            <h1>PROCESO ARTESANAL</h1>
+            <p> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonum- </p>
+            <p> my nibh euismod tincidunt </p>
+           </div>
+          </div>
+          <div class="item">
+           <img src="img/portada-2.png" alt="Portada 2" class="img-responsive" >
+           <div class="carousel-caption">
+            <h1>PROCESO ARTESANAL</h1>
+            <p> my nibh euismod tincidunt </p>
+           </div>
+          </div>
+        </div>
+
+        <!-- Controls -->
+        <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+        
+        <!-- Para la parte izquierda --> <!-- Para la parte izquierda --> <!-- Para la parte izquierda --> <!-- Para la parte izquierda -->
+        <!-- Para la parte izquierda --> <!-- Para la parte izquierda --> <!-- Para la parte izquierda --> <!-- Para la parte izquierda -->
+        <div class="class-enlaces-left" >
+            <div class="class-contect-white" >
+              EN  
+            </div>
+            <div class="class-compras" >
+             <img src="iconos/shop-trolley.svg" style="width:40px;" >   COMPRAR
+            </div>
+            <div class="class-contect-white" > 
+             <img src="iconos/tuna.svg" style="width:40px;" >
+            </div>
+        </div>
+        
+        
+    </div>  
+      
+  </div>   
+        
+  <!-- --> <!-- --> <!-- --> <!-- --> <!-- --> <!-- --> <!-- --> <!-- --> <!-- --> <!-- --> <!-- -->
+  <!-- --> <!-- --> <!-- --> <!-- --> <!-- --> <!-- --> <!-- --> <!-- --> <!-- --> <!-- --> <!-- -->
+  <blockquote class="text-center" >
+   
+    <div class="container-quienes-somos" >
+      <div class="container" >  
+        <h3>¿ QUIÉNES SOMOS ?</h3>
+        <p> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh cuis- </p>       
+        <p> mod tincidunt ur laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim </p>
+        <p> veniam, quis nostrud </p>
+        <br><br>
+        <!-- Boton -->
+        <a class="class-bottom-mas" > Ver más </a>
+      </div>  
+    </div>  
+
+    <div class="container-recomendaciones" >
+     <div class="container" >    
+      <div class="row" >
+       <div class="col-md-4">
+        <p> RECONOCIMIENTOS </p>  
+       </div>
+       <div class="col-md-4">
+        <p> SOSTENIBILIDAD </p>       
+       </div>
+       <div class="col-md-4">
+        <p> NUESTRO PROCESO </p>      
+       </div>  
+      </div>
+     </div>    
+    </div>  
+      
+  </blockquote>
+  
+  
+  
+    
+    
+ <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+ <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script> 
+</body>
+</html>
