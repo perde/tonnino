868c875bc7709303206ea09c84071b02137ba580
Daniel <daniel2864@gmail.com>
2019-06-12 16:28:28 -0400

    espacio footer

--- privacidad.php
+++ privacidad.php
@@ -102,7 +102,7 @@
   </div>
   
   <!-- Footer -->
-  <footer class="class-footer" >
+  <footer class="class-footer" style="margin-top:100px">
   <div class="container" >
   	<div class="row" >
 	 <div class="col-md-4 class-tlite-text" >
